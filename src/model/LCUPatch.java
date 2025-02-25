d2954d2cb6d43568a61a586d261a93db933e3a7f
Hyowon Arzil Bernabe <2244644@slu.edu.ph>
2025-02-25 17:55:16 +0800

    first commit

---
+++ src/model/LCUPatch.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class LCUPatch {
+    public static int patchToClient(String endpoint, String jsonBody) {
+        try {
+            // Construct the request URL
+            String urlString = "https://127.0.0.1:" + LCUAuth.port + endpoint;
+            URL url = new URL(urlString);
+
+            // Open an HTTP connection
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("PATCH");
+            connection.setDoOutput(true); // Enables sending request body
+
+            // Set authentication headers
+            String auth = "riot:" + LCUAuth.token;
+            String encodedAuth = java.util.Base64.getEncoder().encodeToString(auth.getBytes());
+            connection.setRequestProperty("Authorization", "Basic " + encodedAuth);
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            // Send the request body
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = jsonBody.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            // Get the response code
+            int responseCode = connection.getResponseCode();
+            System.out.println("PATCH request to " + endpoint + ", response code: " + responseCode);
+
+            return responseCode;
+        } catch (Exception e) {
+            System.out.println("Error sending PATCH request to " + endpoint);
+            return -1;
+        }
+    }
+}
